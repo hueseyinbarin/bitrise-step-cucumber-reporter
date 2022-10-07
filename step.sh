94d0d4634b7f759ef531e71223c5d9e15b4b7a59
hueseyinbarin <hueseyin.barin@nb-hbarin3.xing.hh>
2022-10-07 18:08:32 +0200

    give direct path

--- step.sh
+++ step.sh
@@ -6,7 +6,6 @@ TMP_CURRENT_DIR="$( pwd )"
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo $THIS_SCRIPT_DIR
-
 cd $THIS_SCRIPT_DIR
 
-java -cp demo.jar org.example.App $BITRISE_DEPLOY_DIR/json  $BITRISE_DEPLOY_DIR/json
+java -cp demo.jar org.example.App $BITRISE_SOURCE_DIR/tooling/resources  $BITRISE_DEPLOY_DIR/
